843d9a87e8e700a2f393d980bfbc8aca4943fd71
Celina Jackson <celinanjackson@gmail.com>
2020-07-22 19:13:16 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,6 +12,11 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] =  {}
 end
 
-create_project_hash
+projects
+
+end
